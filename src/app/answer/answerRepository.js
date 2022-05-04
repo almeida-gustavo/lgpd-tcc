0726e0752d7aed46ac8be6809ad9b79aac429bce
melogustavo <gustavomelo95@gmail.com>
2022-05-03 21:15:28 -0300

    update response from list awnsers

--- src/app/answer/answerRepository.js
+++ src/app/answer/answerRepository.js
@@ -21,7 +21,41 @@ class AnswerRepository {
       ],
     });
 
-    return results;
+    const formattedResult = new Map();
+
+    results.forEach(result => {
+      const formatedObject = {
+        options: result.options,
+        id: result.id,
+        question: result.question,
+        answer: (result.answers[0] || {}).answer || null,
+      };
+
+      if (formattedResult.has(result.section)) {
+        const existingValues = formattedResult.get(result.section);
+        formattedResult.set(result.section, [
+          ...existingValues,
+          formatedObject,
+        ]);
+      } else {
+        formattedResult.set(result.section, [formatedObject]);
+      }
+    });
+
+    const iteratorResult = formattedResult[Symbol.iterator]();
+
+    const finalResult = [];
+
+    /* eslint-disable no-restricted-syntax */
+    for (const item of iteratorResult) {
+      console.log(item);
+      finalResult.push({
+        section: item[0],
+        questions: item[1],
+      });
+    }
+
+    return finalResult;
   }
 
   async updateAnswers(req) {
