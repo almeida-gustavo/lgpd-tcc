6d86fffe222f933be9acbd1d39d2029835f5305a
melogustavo <gustavomelo95@gmail.com>
2022-06-04 12:43:07 -0300

    add extra validation to login

--- src/app/sessionControler/sessionService.js
+++ src/app/sessionControler/sessionService.js
@@ -16,6 +16,10 @@ class UserService {
 
     const { email, password } = req.body;
 
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Missing email or password' });
+    }
+
     const user = await User.findOne({
       where: { email },
     });
